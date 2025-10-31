72663b785d363d5096090276029c704bc5f774da
TheTHUNDER8 <hugo.fg13@gmail.com>
2025-10-31 12:02:04 +0000

    teste assests

---
+++ vetgestlink/common/models/Userprofile.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "userprofiles".
+ *
+ * @property int $id
+ * @property string $nomecompleto
+ * @property string $nif
+ * @property string $telemovel
+ * @property string $dtanascimento
+ * @property string $dtaregisto
+ * @property int $user_id
+ * @property int $eliminado
+ *
+ * @property Animai[] $animais
+ * @property Fatura[] $faturas
+ * @property Marcaco[] $marcacos
+ * @property Morada[] $moradas
+ * @property Nota[] $notas
+ * @property User $user
+ */
+class Userprofile extends \yii\db\ActiveRecord
+{
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'userprofiles';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['eliminado'], 'default', 'value' => 0],
+            [['nomecompleto', 'nif', 'telemovel', 'dtanascimento', 'dtaregisto', 'user_id'], 'required'],
+            [['dtanascimento', 'dtaregisto'], 'safe'],
+            [['user_id', 'eliminado'], 'integer'],
+            [['nomecompleto'], 'string', 'max' => 45],
+            [['nif', 'telemovel'], 'string', 'max' => 9],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nomecompleto' => 'Nomecompleto',
+            'nif' => 'Nif',
+            'telemovel' => 'Telemovel',
+            'dtanascimento' => 'Dtanascimento',
+            'dtaregisto' => 'Dtaregisto',
+            'user_id' => 'User ID',
+            'eliminado' => 'Eliminado',
+        ];
+    }
+
+    /**
+     * Gets query for [[Animais]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAnimais()
+    {
+        return $this->hasMany(Animai::class, ['userprofiles_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Faturas]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getFaturas()
+    {
+        return $this->hasMany(Fatura::class, ['userprofiles_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Marcacos]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getMarcacos()
+    {
+        return $this->hasMany(Marcaco::class, ['userprofiles_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Moradas]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getMoradas()
+    {
+        return $this->hasMany(Morada::class, ['userprofiles_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Notas]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getNotas()
+    {
+        return $this->hasMany(Nota::class, ['userprofiles_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[User]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::class, ['id' => 'user_id']);
+    }
+
+}
---
+++ vetgestlink/frontend/controllers/UserProfileController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace frontend\controllers;
+
+use common\models\Userprofile;
+use Yii;
+use yii\data\ActiveDataProvider;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * UserProfileController implements the CRUD actions for Userprofile model.
+ */
+class UserProfileController extends Controller
+{
+    /**
+     * @inheritDoc
+     */
+    public function behaviors()
+    {
+        return array_merge(
+            parent::behaviors(),
+            [
+                'verbs' => [
+                    'class' => VerbFilter::className(),
+                    'actions' => [
+                        'delete' => ['POST'],
+                    ],
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Lists all Userprofile models.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => Userprofile::find(),
+            /*
+            'pagination' => [
+                'pageSize' => 50
+            ],
+            'sort' => [
+                'defaultOrder' => [
+                    'id' => SORT_DESC,
+                ]
+            ],
+            */
+        ]);
+
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single Userprofile model.
+     * @param int $id ID
+     * @return string
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+
+    /**
+     * Updates an existing Userprofile model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param int $id ID
+     * @return string|\yii\web\Response
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+
+    /**
+     * Finds the Userprofile model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param int $id ID
+     * @return Userprofile the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Userprofile::findOne(['id' => $id])) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+}
