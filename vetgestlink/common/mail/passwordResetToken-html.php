ddded6dec954f73d85afa861898cea81ab5e3ee5
MrMagmaPT <pmrmagma@gmail.com>
2026-01-05 19:08:29 +0000

    Use Yii URL manager for password reset links
    Replaced hardcoded localhost URLs with Yii's urlManager->createAbsoluteUrl for generating password reset links in both HTML and text email templates. This ensures correct URL generation across different environments.

--- vetgestlink/common/mail/passwordResetToken-html.php
+++ vetgestlink/common/mail/passwordResetToken-html.php
@@ -5,9 +5,7 @@ use yii\helpers\Html;
 /** @var yii\web\View $this */
 /** @var common\models\User $user */
 
-//Mudei isso para sempre ser redirecionado para o frontend para reset de password
-//TODO mudar em produção localhost para o domínio real
-$resetLink = 'http://localhost/vetgestlink/frontend/web/site/reset-password?token=' . $user->password_reset_token;
+$resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password?token=' . $user->password_reset_token]);
 ?>
 <div class="password-reset">
     <p>Olá <?= Html::encode($user->username) ?>,</p>
--- vetgestlink/common/mail/passwordResetToken-text.php
+++ vetgestlink/common/mail/passwordResetToken-text.php
@@ -3,9 +3,7 @@
 /** @var yii\web\View $this */
 /** @var common\models\User $user */
 
-//Mudei isso para sempre ser redirecionado para o frontend para reset de password
-//TODO mudar em produção localhost para o domínio real
-$resetLink = 'http://localhost/vetgestlink/frontend/web/site/reset-password?token=' . $user->password_reset_token;
+$resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password?token=' . $user->password_reset_token]);
 ?>
 Olá <?= $user->username ?>,
 
